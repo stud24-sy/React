15dc6fe5d6d94b1f8e7ca94304eb82a120d16aaf
이 세영 <lys200209@naver.com>
2023-08-19 17:46:16 +0900

    feat: apply Ant Design + Tailwind CSS

---
+++ my-app/src/components/modal/ModalTitleBox.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Divider} from "antd";
+
+const ModalTitleBox = () => {
+    return (
+        <section className="title">
+            <span className="font-bold text-xl text-gray-700">영업 기회 등록</span>
+            <Divider />
+        </section>
+    );
+};
+
+export default ModalTitleBox;
