a73b24b2f3e459fd409d83974cedae157cba8d08
이 세영 <lys200209@naver.com>
2023-08-26 23:43:19 +0900

    feat: apply Ant Design + Tailwind CSS

--- my-app/src/components/modal/ModalBox.js
+++ my-app/src/components/modal/ModalBox.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Modal, Divider} from "antd";
 import ModalTitleBox from './ModalTitleBox';
 import ModalCustomerInfoBox from './ModalCustomerInfoBox';
 import ModalBusinessInfoBox from './ModalBusinessInfoBox';
 import ModalFooterBox from './ModalFooterBox';
+import axios from "axios";
 
 const ModalBox = ({ modalVisible, selectedRowData, closeModal }) => {
+    const [user, setUser] = useState({
+        userId: '',
+        category: '',
+        content: ''
+    });
+
+    function testFunction() {
+        axios.get(`/api/host`)
+            .then((response) => {
+                alert("사용자 정보를 불러왔습니다. : " + response.data);
+                setUser(response.data);
+            })
+            .catch((error) => {
+                alert("사용자 정보 불러오기 실패 : " + JSON.stringify(error.response.data));
+                console.log(error);
+            });
+    }
+
     return (
         <Modal
             visible={modalVisible}
@@ -17,9 +36,14 @@ const ModalBox = ({ modalVisible, selectedRowData, closeModal }) => {
             {selectedRowData && (
                 <div>
                     <ModalTitleBox />
-                    <ModalCustomerInfoBox />
-                    <ModalBusinessInfoBox />
+                    <ModalCustomerInfoBox
+                        user={user}
+                        setUser={setUser}
+                    />
+                    <ModalBusinessInfoBox/>
                     <ModalFooterBox
+                        user={user}
+                        setUser={setUser}
                         closeModal={closeModal}
                     />
                 </div>
