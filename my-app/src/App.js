1f86baaab2e4f07174bc0a01ef254fa34978ed80
이 세영 <lys200209@naver.com>
2023-09-09 14:40:23 +0900

    feat: Recoil state 사용

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import TitleBox from './components/TitleBox';
 import SearchBox from './components/SearchBox';
 import ProgressBox from './components/ProgressBox';
 import ResultBox from './components/ResultBox';
 import './App.css';
+import {useRecoilState} from "recoil";
+import { contentStore } from './store';
 
 const App = () => {
+
+    const [content, setContent] = useRecoilState(contentStore);
+    const [user, setUser] = useState({
+        userId: '',
+        category: '',
+        content: ''
+    });
+    // const [clientName, setClientName] = useRecoilState(clientNameState);
+    // const [shipToName, setShipToName] = useRecoilState(shipToNameState);
+
+    const handleButton_onClick = () => {
+        setContent({
+            clientName: 'Seyeong',
+            shipToName: '신규 사용자입니다'
+        })
+    }
+
+    // const handleClientNameChange = (e) => {
+    //     setClientName(e.target.value);
+    // };
+
+    // const handleShipToNameChange = (e) => {
+    //     setShipToName(e.target.value);
+    // };
+
     return (
         <>
             <TitleBox />
