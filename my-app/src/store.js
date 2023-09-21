61001107ecb7c137f3488c3af3baf89e0fa7f9dc
이 세영 <lys200209@naver.com>
2023-09-21 23:52:36 +0900

    feat: FontAwesomeIcon 사용

--- my-app/src/store.js
+++ my-app/src/store.js
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import {atom} from 'recoil';
 
 /**
  * Client Name, Ship To Name 저장소
@@ -8,7 +8,7 @@ export const contentStore = atom({
     key: 'contentStore',
     default: {
         clientName: '저장소 값',
-        shipToName: '저장소 값'
+        shipToName: '저장소 값',
     }
 });
 
@@ -16,6 +16,8 @@ export const customerContentStoreAtom = atom({ // 변수 이름 변경
     key: 'customerContentStore',
     default: {
         clientName: '',
-        shipToName: ''
+        shipToName: '',
+        soldToName: '',
+        billToName: ''
     },
 });
